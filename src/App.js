12124a620a9381ce4e32cb2a708b9c6ba149126f
IgorFerreiraSilva <igor_ferreira_silva1@hotmail.com>
2023-07-09 12:56:48 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      textoFrase : '',
+    }
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+
+      this.frases = ['Apressa-te a viver bem e pensa que cada dia é, por si só, uma vida.  Sêneca',
+      '“A riqueza não consiste em ter grandes posses, mas em ter poucas necessidades.” Epicteto',
+      '“Você tem poder sobre sua mente não sobre eventos externos. Perceba isso e você encontrará a sua força.” Marco Aurélio',
+      '“Sorte é o que acontece quando a preparação encontra a oportunidade“. Sêneca',
+      '“Todos nós podemos errar, mas a perseverança no erro é a verdadeira loucura“. Zenão',
+      '“A felicidade de sua vida depende da qualidade de seus pensamentos.” Marco Aurélio',
+      '“Quem não se contenta com pouco, não se contenta com nada.” Epicuro',
+      '“O amigo é um segundo eu“. Zenão',
+      '“As dificuldades fortalecem a mente, assim como o trabalho o faz com o corpo“. Sêneca',
+      '“Pense na beleza da vida. Observe as estrelas e veja-se correndo com elas.” Marco Aurélio'
+    ]
+
+  }
+
+  quebraBiscoito(){
+    let state = this.state
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+    state.textoFrase = this.frases[numeroAleatorio]
+    this.setState(state)
+  }
+
+  render(){
+    return (
+      <>
+
+      <div className="container">
+           <img src={require('./assets/biscoito.png')} className="img" />
+              <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>      
+                <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+
+      </>
+    );
+  }
+}
+
+
+class Botao extends Component{
+  render(){
+    return <>
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+    </>
+  }
 }
 
 export default App;
